bea9b6b04dc845877232cade2e8e68f0c4a91128
Alejandro Di Leo <ale.dileo17@gmail.com>
2019-09-14 14:25:56 -0300

    Adds zeit color theme

---
+++ theme.js
@@ -0,0 +1,27 @@
+export default {
+  primary: {
+    background: '#000',
+    foreground: '#FFF'
+  },
+  error: {
+    light: '#FF3333',
+    default: '#FF0000',
+    dark: '#E60000' 
+  },
+  success: {
+    light: '#3291FF',
+    default: '#0070F3',
+    dark: '#0366D6'
+  },
+  warning: {
+    light: '#F7B955',
+    default: '#F5A623',
+    dark: '#F49B0B'
+  },
+  highlight: {
+    alert: '#FF0080',
+    purple: '#F81CE5',
+    violet: '#7928CA',
+    cyan: '#79FFE1'
+  }
+};
